21ce8ef9791e55026c1d4c2bbe1664530fbbaeba
Zoomichi <47381251+Zoomichi@users.noreply.github.com>
2022-10-17 19:14:30 +0800

    Update search.py

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,36 @@ def search_users(args):
     """
 
     # Implement search here!
+    if args == {}:
+        return USERS
+    
+    result = []
+    isAdded = {}
+    for key, val in args.items():
+        for user in USERS:
+            if key == "id":
+                if user[key] == val:
+                    if user["id"] not in isAdded:
+                        isAdded[user["id"]] = True
+                        result.append(user)
+                    elif isAdded[user["id"]] == True:
+                        pass                        
+            elif key == "name" or key == "occupation":
+                if val.lower() in user[key].lower():
+                    if user["id"] not in isAdded:
+                        isAdded[user["id"]] = True
+                        result.append(user)
+                    elif isAdded[user["id"]] == True:
+                        pass                        
+            elif key == "age":
+                if user[key] <= int(val)+1 and user[key] >= int(val)-1:
+                    if user["id"] not in isAdded:
+                        isAdded[user["id"]] = True
+                        result.append(user)
+                    elif isAdded[user["id"]] == True:
+                        pass
 
-    return USERS
+    if result == []:
+        return USERS
+    else:
+        return result
